be1aaeb343f4ec7c4aeb11f69523fb1917171568
kwanghoon.kim <kk03187693@gmail.com>
2020-12-09 00:57:14 +0900

    add QueryDSL setting

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,39 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+//    @PersistenceContext : 자바 표준 스택, 스프링 최신 버전에서는 Autowired 사용해도 된다.
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
-    }
+        Hello hello = new Hello();
+        em.persist(hello);
 
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = new QHello("h");
+
+        // command + option + v: 변수 만든 후 넣어 줌
+        Hello result = query
+            .selectFrom(qHello)
+            .fetchOne();
+
+        // static import: option + enter
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
+    }
 }
