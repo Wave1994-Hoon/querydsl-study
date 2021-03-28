100354f57bcf4d44be86b8591a151cda9c6ebb6c
kwanghoon.kim <kk03187693@gmail.com>
2021-03-28 22:15:05 +0900

    add join example

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -192,4 +192,45 @@ public class QuerydslBasicTest {
         assertThat(teamA.get(team.name)).isEqualTo("teamA");
         assertThat(teamA.get(member.age.avg())).isEqualTo(15);
     }
+
+    /**
+     * 팀 A에 소속된 모든 회원
+     */
+    @Test
+    public void join() {
+        QTeam team = QTeam.team;
+
+        List<Member> result = queryFactory
+            .selectFrom(member)
+            .join(member.team, team) // inner join 같음
+            .where(team.name.eq("teamA"))
+            .fetch();
+
+        assertThat(result)
+            .extracting("username")
+            .containsExactly("member1", "member2");
+    }
+
+    /**
+     * 세타 조인 -> 연관관계가 없는 필드로 조인
+     * 회원의 이름이 팀 이름과 같은 회원 조회
+     *
+     * from 절에 여러 엔티티를 선택해서 세타 조인
+     * 외부 조인 불가능하지만 on 절을 사용하면 가능함 -> 하이버네이트에서 지원하기 시작함
+     */
+    @Test
+    public void theta_join() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+
+        QTeam team = QTeam.team;
+
+        queryFactory
+            .select(member)
+            .from(member.team, team)
+            .where(member.username.eq(team.name))
+            .fetch();
+
+
+    }
 }
